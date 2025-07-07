a6c2672a2d9db130bf4bcc26364b6da61b419cba
mmapro12 <asia172007@gmail.com>
2025-07-07 14:27:49 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /venv/bin/activate
-pyinstaller --onefile=main.py 
+pyinstaller --onefile main.py 
 sudo mv ./dist/main /usr/local/bin/durmake
 sudo chmod +x /usr/local/bin/durkmake
 
