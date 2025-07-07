127b06db9cf5f75eb0f352b6c9cdb3c0bd34a093
mmapro12 <asia172007@gmail.com>
2025-07-07 14:29:39 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 source /venv/bin/activate
 pyinstaller --onefile main.py 
 sudo mv ./dist/main /usr/local/bin/durmake
-sudo chmod +x /usr/local/bin/durkmake
+sudo chmod +x /usr/local/bin/durmake
 
 echo "İndirme tamamlandı..."
 
